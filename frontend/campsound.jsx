2efa3a3ad58df35d99efc840dd97620a82a9ae97
“ericathompson” <ericathompson.nc@gmail.com>
2020-12-07 15:48:16 -0800

    Add Root component rendering

--- frontend/campsound.jsx
+++ frontend/campsound.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { signup, login, logout } from './util/session_api_util';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to CampSound! 🏕</h1>, root);
+
+    ReactDOM.render(<Root/>, root);
 });
 
 window.signup = signup;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Root extends React.Component {
+
+    render(){
+        return(
+            <h1>🏕 CampSound </h1>
+        )
+    }
+
+}
+
+export default Root;

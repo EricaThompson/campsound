9ccc377458e012f8bfd05e88aa9b25c4c9ef2b88
“ericathompson” <ericathompson.nc@gmail.com>
2020-12-07 14:36:53 -0800

    Add session api utils

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -11,7 +11,7 @@ class Api::SessionsController < ApplicationController
 
     def destroy
         if !current_user 
-            render json: ['No user'], status: 404
+            render json: ['No user is signed in!'], status: 404
         end
         logout!
         render json: {}

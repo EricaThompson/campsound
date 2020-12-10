a8e968fa9713e33c920e62eeab6ac86389c5c6d5
“ericathompson” <ericathompson.nc@gmail.com>
2020-12-09 19:20:52 -0800

    Add Item model"

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id            :bigint           not null, primary key
+#  owner_id      :integer          not null
+#  title         :string           not null
+#  genre         :string
+#  price         :string
+#  released      :boolean          not null
+#  about         :text
+#  collection_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Item < ApplicationRecord
+    validates :owner_id, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :released, presence: true, inclusion: [true, false]
+
+    belongs_to :user,
+        foreign_key: :owner_id,
+        class_name: :User
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id            :bigint           not null, primary key
+#  owner_id      :integer          not null
+#  title         :string           not null
+#  genre         :string
+#  price         :string
+#  released      :boolean          not null
+#  about         :text
+#  collection_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase

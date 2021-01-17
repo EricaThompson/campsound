52a875d632211dc6f171cde65c3f7de7e6c23748
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-17 14:55:22 -0800

    Add more styling to stories index, add latest section

--- frontend/components/stories/stories_index.jsx
+++ frontend/components/stories/stories_index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 // import Item from './item';
 import { Link } from 'react-router-dom';
 
+import latest from '../../../app/assets/images/latest.png';
+
 class StoriesIndex extends React.Component {
     constructor(props) {
         super(props);
@@ -19,7 +21,10 @@ class StoriesIndex extends React.Component {
             url: `/api/stories`,
             method: 'GET'
         }).then(res => this.setState({ mainStory: Object.values(Object.values(res))[0]}))
-        .then(res => this.setState({ storyList: Object.values(Object.values(res))}))
+        $.ajax({
+            url: `/api/stories`,
+            method: 'GET'
+        }).then(res => this.setState({ storyList: Object.values(Object.values(res))}))
         // }).then(res => console.log(Object.values(Object.values(res))))
         // }).then(res => console.log(Object.values(Object.values(res))[0].text))
     }
@@ -73,61 +78,30 @@ class StoriesIndex extends React.Component {
 
 
     render() {
-        console.log('props', this.props)
+        console.log('storyList', this.state.storyList)
 
-        let storyDisplay = this.state.storyList.map((story) => {
+        // let storyListMinusMain = this.state.storyList.slice(1)
+
+        let storyDisplay = this.state.storyList.slice(1).map((story) => {
             return <div key={story.id} className="story-display">
-               
-                <Link to={`artists/${this.props.currentUserId}/music/${story.id}`}>
+                <div className="story-image"></div>
+                <div className='main-story-date'>
+                    <span className='main-story-type'>{story.type}</span> · {story.date}
+                </div>
+               {/* <h5 >{story.type} · {story.date}</h5> */}
+                {/* <Link to={`stories/${this.props.currentUserId}/story/${story.id}`}> */}
                     {/* <img src={`${story.title}`} alt="" /> */}
-                    <h5>{story.title}</h5>
-                    <h5 className="home-text top">{story.text}</h5>
-                </Link>
-               
+                    <h5 className='main-story-title'>{story.title}</h5>
+                    {/* <div className="main-story-summary">{story.text}</div> */}
+
+                {/* </Link> */}
+                {/* <div className='main-story-author'>By {story.author}</div> */}
                 {/* <h5 onClick={() => this.addTostoryList(story)} className="home-text add">Add to Playlist</h5> */}
                 
 
             </div>
         })
-        let current = 'current';
-
-        // let playlist = this.state.storyList.map((song, idx) => {
-        //     return <div key={Math.random()} className={`i${idx + 1}`}>
-        //         {/* {indices.push(idx)} */}
-        //         <div onClick={() => this.setState({ playerView: false }, () => this.playSong(song.song))}>
-        //             ▶
-        //                 </div>
-        //         <div>
-        //             {idx + 1 + "."}
-        //         </div>
-
-        //         <div
-        //             className={current}
-        //         >
-        //             {song.title} by {song.artist}
-        //         </div>
-        //         <div>
-
-        //         </div>
-        //         <div onClick={() => this.removeSong(song)}>
-        //             ✘
-        //                 </div>
-        //     </div>
-        // })
-
-        // let player = <audio key={this.state.currentSong} onEnded={() => this.nextSong(this.state.currentSong)} autoPlay={true} id="single-player" controls>
-        //     <source src={this.state.currentSong} type="audio/mpeg" />
-        //                 Your browser does not support the audio tag.
-        //             </audio>
-
-
-        // if (!this.state.playerView || Object.values(this.state.itemList).length === 0) {
-        //     player = null
-        // }
-        // let playerTitle;
-        // if (Object.values(this.state.itemList).length > 0) {
-        //     playerTitle = <div className="playlistTitle">playlist</div>
-        // }
+        // let current = 'current';
 
 
         return (
@@ -155,14 +129,20 @@ class StoriesIndex extends React.Component {
                             {this.state.mainStory.text}
                         </div>
                         <div className='main-story-author'>
-                            By {this.state.mainStory.author}
+                            By <span className='name'>{this.state.mainStory.author}</span>
                         </div>
                     </div>
+                </div>
+                
+                    <div><img className='latest' src={latest} alt="" /></div>
+                <div className='latest-stories'>
+                    
+                    {storyDisplay}
+
                 </div>
 
                 {/* <h1 className="stories-title">Stories</h1>
                 <h4>Count of items</h4> */}
-                {storyDisplay}
             </div>
         )
     }

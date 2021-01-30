20b451e3d0c8c31052dde1b9b88546d5b6046463
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-30 12:05:50 -0800

    Add most of about page asset credits

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -26,24 +26,68 @@ class About extends React.Component {
                 </h4>
                 <h3>Assets</h3>
                 <div className='credits'>
-
-                    <a href="https://www.pexels.com/@evie-shaffer-1259279">Evie Shaffer</a>
+                    <a target='_blank' href="https://www.pexels.com/@evie-shaffer-1259279">Evie Shaffer</a>
                     <ul>
                         <li><a target="_blank" href="https://www.pexels.com/photo/brown-fern-leaves-2512282/">Brown Fern Leaves</a></li>
                         <li><a target='_blank' href="https://www.pexels.com/photo/green-petaled-flower-2395250/">Green-Petaled Flower</a></li>
                         <li><a target='_blank' href="https://www.pexels.com/photo/green-leafed-plant-with-pink-flowers-2395240/">Green-Leafed Plant with Pink Flowers</a></li>
                         <li><a target='_blank' href="https://www.pexels.com/photo/leaves-on-white-surface-3585648/">Leaves of White Surface</a></li>
                     </ul>
-                    <a href="https://www.pexels.com/@tracehudson">Trace Hudson</a>
+
+                    <a target='_blank' href="https://www.pexels.com/@tracehudson">Trace Hudson</a>
                     <ul>
                         <li><a href="https://www.pexels.com/photo/bird-s-eye-view-of-mountains-during-dawn-2801818/">Bird's-Eye View of Mountains During Dawn</a></li>
                         <li><a href="https://www.pexels.com/photo/snow-covered-mountain-2365457/">Snow Covered Mountain</a></li>
                     </ul>
-                    <a href="">Scott Webb</a>
+
+                    <a target='_blank' href="https://www.pexels.com/@scottwebb">Scott Webb</a>
                     <ul>
                         <li><a target='_blank' href="https://www.pexels.com/photo/white-architectural-structure-1022928/">White Architectural Structure</a></li>
                         <li><a target='_blank' href="https://www.pexels.com/photo/purple-and-blue-abstract-wallpaper-430207/">Purple and Blue Abstract Wallpaper</a></li>
                     </ul>
+
+                    <a target='_blank' href="https://www.pexels.com/@aronvisuals">Aron Visuals</a>
+                    <ul>
+                        <li><a target='_blank' href="https://www.pexels.com/photo/brown-mountains-1643113/">Brown Mountains</a></li> 
+                    </ul>
+
+                    <a target='_blank' href="https://www.pexels.com/@efdal-yildiz-325089">Efdal YILDIZ</a>
+                    <ul>
+                        <li><a href="https://www.pexels.com/photo/flock-of-birds-917494/">Flock of Birds</a></li>
+                    </ul>
+
+                    <a target="_blank" href="https://www.pexels.com/@mauveine">Sunyu Kim</a>
+                    <ul>
+                        <li><a href="https://www.pexels.com/photo/flock-of-birds-917494/">Body of Water and Mountains</a></li>
+                    </ul>
+
+                    <a target="_blank" href="https://www.pexels.com/@kei-scampa-1201427">Kei Scampa</a>
+                    <ul>
+                        <li><a href="https://www.pexels.com/photo/green-wheat-field-4507967/">Green Wheat Field</a></li>
+                    </ul>
+
+                    <a target="_blank" href="https://www.pexels.com/@katlovessteve">Kat Jayne</a>
+                    <ul>
+                        <li><a href="https://www.pexels.com/photo/yellow-plant-545313/">Yellow Plant</a></li>
+                    </ul>
+
+                    <a href="https://www.freepik.com/macrovector">MACROVECTOR</a>
+                    <ul>
+                        <li><a href="https://www.freepik.com/vectors/vintage">Retro Gramophone Icons Set</a></li>
+                    </ul>
+
+                    <a href="https://www.freepik.com/alvaro-cabrera">Alvaro Cabrera</a>
+                    <ul>
+                        <li><a href="https://www.freepik.com/vectors/music">Microphones Design Collection</a></li>
+                    </ul>
+
+                    <a href="https://www.freepik.com/freepik">Freepik</a>
+                    <ul>
+                        <li><a href="https://www.freepik.com/free-vector/hand-drawn-gadget-collection_877589.htm#page=1&query=headphones&position=14">Hand-Drawn Gadget Collection</a></li>
+                    </ul>
+
+                    
+                    
                 </div>
 
             </div>

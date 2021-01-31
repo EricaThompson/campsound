31ef65e4146b593f5b13a9a24d77d91f4e8a1404
“ericathompson” <ericathompson.nc@gmail.com>
2021-01-30 17:31:50 -0800

    Add, complete next and previous story buttons

--- frontend/components/stories/story.jsx
+++ frontend/components/stories/story.jsx
@@ -11,6 +11,8 @@ class Story extends React.Component {
             news: "https://images.pexels.com/photos/1022928/pexels-photo-1022928.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=620",
             spinnerShow: false,
             nextStory: null,
+            previousStory: null,
+            currentIdx: null, 
 
         }
     }
@@ -19,21 +21,43 @@ class Story extends React.Component {
         this.setState({spinnerShow: true})
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
+        // let nextIdx = this.props.match.params.storyId + 1
+        // console.log(this.props.match.params.storyId)
+        
         $.ajax({
             url: `/api/users/${this.props.match.params.ownerId}/stories/${this.props.match.params.storyId}`,
             method: 'GET'
         }).then(res => {
-            this.setState({ story: res })
+            this.setState({ story: res, currentIdx: res.id })
             this.setState({spinnerShow: false})
         })
+        let idx = parseInt(this.props.match.params.storyId)
+        // console.log('id',this.props.match.params.storyId)
+
+        console.log(idx)
+        $.ajax({
+            url: `/api/stories/${idx + 1}`,
+            method: 'GET'
+        }).then(res => {
+            this.setState({ previousStory: res })
+            console.log('prev',this.state.previousStory)
+            // this.setState({ spinnerShow: false })
+        })
+
+        let nextIdx = idx + 1
+        console.log('nextidx', nextIdx) 
 
         $.ajax({
-            url: `/api/users/${this.props.match.params.ownerId}/stories/${this.props.match.params.storyId - 1}`,
+            url: `/api/stories/${idx - 1}`,
             method: 'GET'
         }).then(res => {
             this.setState({ nextStory: res })
+            console.log('next:',this.state.nextStory)
             // this.setState({ spinnerShow: false })
         })
+
+
+
 
         
 
@@ -64,10 +88,12 @@ class Story extends React.Component {
         }
 
         let nextBtn;
+        let previousBtn;
 
-        if (this.state.nextStory){
+        if (this.state.nextStory && !this.state.previousStory){
+
             nextBtn = <i 
-                            className="next-btn link fas fa-play"
+                            className="next-btn link fas fa-play next-solo"
                             onClick={
                             () => {
                                 this.props.history.replace(`/users/${this.state.story.owner_id}/stories/${this.state.nextStory.id}`)
@@ -76,7 +102,43 @@ class Story extends React.Component {
                             }>
                         
                         </i>
-                    // </button>
+                
+        } else if (this.state.nextStory){
+            nextBtn = <i
+                className="next-btn link fas fa-play"
+                onClick={
+                    () => {
+                        this.props.history.replace(`/users/${this.state.story.owner_id}/stories/${this.state.nextStory.id}`)
+                        window.location.reload()
+                    }
+                }>
+
+            </i>
+        }
+
+        if (this.state.previousStory && !this.state.nextStory) {
+            previousBtn = <i
+                className="previous-btn link fas fa-play previous-solo"
+                onClick={
+                    () => {
+                        this.props.history.replace(`/users/${this.state.story.owner_id}/stories/${this.state.previousStory.id}`)
+                        window.location.reload()
+                    }
+                }>
+
+            </i>
+            // </button>
+        } else if (this.state.previousStory){
+            previousBtn = <i
+                className="previous-btn link fas fa-play"
+                onClick={
+                    () => {
+                        this.props.history.replace(`/users/${this.state.story.owner_id}/stories/${this.state.previousStory.id}`)
+                        window.location.reload()
+                    }
+                }>
+
+            </i>
         }
 
         let img;
@@ -116,6 +178,7 @@ class Story extends React.Component {
                         <div className='story-show-title'>{this.state.story.title}</div>
                         <div className='story-show-summary'>{this.state.story.summary}</div>
                         <div className='story-show-author'>By <span className="author" onClick={() => this.props.history.replace(`/users/${this.state.story.owner_id}/stories`)}>{this.state.story.username}</span> · {this.state.story.date}</div>
+                        {previousBtn}
                         {nextBtn}
                         <div className='story-show-img'><img src={img} alt=""/></div>
                         <div className='story-show-text'>{this.state.story.text}</div>

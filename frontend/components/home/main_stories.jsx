fb1998524ee8e25f229ba58840e5d7fd0dc2df6a
“ericathompson” <ericathompson.nc@gmail.com>
2021-02-04 22:29:55 -0800

    Change text to white in main stories, change images

--- frontend/components/home/main_stories.jsx
+++ frontend/components/home/main_stories.jsx
@@ -52,6 +52,7 @@ class MainStories extends React.Component {
                         className="main link">
                         <p>The Forest Through the Trees</p>
                         <h6>{summary0}</h6>
+                        <button className="read-more">read more</button>
                         <img src="https://campsound-dev.s3-us-west-1.amazonaws.com/assets/images/home-stories-main.jpg" alt="blue flowers" />
                     </div>
 
